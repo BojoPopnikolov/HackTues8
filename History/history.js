43d772c2a34e7b6467beaf82fc83aa0ce86a18a3
BojoPopnikolov <bojo1556@gmail.com>
2022-03-13 07:38:44 +0200

    rmv history page from nav

--- History/history.js
+++ History/history.js
@@ -8,7 +8,7 @@ var nodeId;
 var xhr = new XMLHttpRequest;
 var searchURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=true&exintro&exsentences=5&origin=*&format=json&titles=";
 
-var url = searchURL + term;
+var url = searchURL + ter;
   
 var p_title;
 var p_content;
@@ -42,7 +42,25 @@ window.onload = () => {
         var term = `${nodeClass}_${nodeId}`;
 
         console.log(term);
+        xhr.open('GET', url, true);
+  
+xhr.onload = function(){
+    var data = JSON.parse(this.response);
+    var pageId = Object.keys(data.query.pages)
+        
+    p_title = data.query.pages[pageId].title;
+    p_content = data.query.pages[pageId].extract;
 
-        wikiContent(term);
+    console.log(p_title, p_content);
+
+    console.log(element, url, data.query.pages[pageId]);
+    
+    document.getElementsByClassName(".heading").innerHTML = p_title;
+    document.getElementsByClassName(".content").innerHTML = p_content;
+}
+  
+xhr.send();
+
+
     }
 }
